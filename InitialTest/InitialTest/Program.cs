0206f69cc7134e96624e9a5c5ea49e036711efbc
Christian Bock <christian.bock1983@gmail.com>
2018-01-20 12:52:19 +0100

    HW

--- InitialTest/InitialTest/Program.cs
+++ InitialTest/InitialTest/Program.cs
@@ -10,6 +10,9 @@ namespace InitialTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
         }
     }
 }
